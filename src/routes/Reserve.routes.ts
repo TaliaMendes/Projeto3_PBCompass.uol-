6f804ce8c2490d85ebe5aa518a6b1f0370eda510
TaliaMendes <taliafmendes09@gmail.com>
2024-07-06 14:53:37 -0300

    feat:Proteger rotas reserve com autenticação JWT

--- src/routes/Reserve.routes.ts
+++ src/routes/Reserve.routes.ts
@@ -3,15 +3,16 @@ import express from 'express';
 import { container } from 'tsyringe';
 import validateObjectId from '../middlewares/Validate.objectId.middewares';
 import validateReserve from '../middlewares/Reserve.middlewares';
+import { checkToken } from '../middlewares/Authenticate.middlewares';
 
 const routes = express.Router();
 const reserveController = container.resolve(ReserveController);
 
-routes.get('/reserve', reserveController.listAllReserves.bind(reserveController));
-routes.get('/reserve/:_id', validateObjectId, reserveController.getReserveById.bind(reserveController));
-routes.post('/reserve', validateReserve, reserveController.createReserve.bind(reserveController));
-routes.put('/reserve/:_id', validateObjectId, validateReserve, reserveController.updateReserve.bind(reserveController));
-routes.delete('/reserve/:_id', validateObjectId, reserveController.removeReserve.bind(reserveController));
+routes.get('/reserve', checkToken, reserveController.listAllReserves.bind(reserveController));
+routes.get('/reserve/:_id', checkToken, validateObjectId, reserveController.getReserveById.bind(reserveController));
+routes.post('/reserve', checkToken, validateReserve, reserveController.createReserve.bind(reserveController));
+routes.put('/reserve/:_id', checkToken, validateObjectId, validateReserve, reserveController.updateReserve.bind(reserveController));
+routes.delete('/reserve/:_id', checkToken, validateObjectId, reserveController.removeReserve.bind(reserveController));
 
 
 
