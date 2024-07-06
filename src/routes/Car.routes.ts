c9f99acb79a2d88c880bb636f7c9f71740a39e7e
TaliaMendes <taliafmendes09@gmail.com>
2024-07-06 14:54:34 -0300

    feat:Proteger rotas cars com autenticação JWT

--- src/routes/Car.routes.ts
+++ src/routes/Car.routes.ts
@@ -3,15 +3,16 @@ import CarsController from '../controllers/Car.controller';
 import { container } from 'tsyringe';
 import validateCar from '../middlewares/Car.middlewares';
 import validateObjectId from '../middlewares/Validate.objectId.middewares';
+import { checkToken } from '../middlewares/Authenticate.middlewares';
 
 const routes = express.Router();
 const carController = container.resolve(CarsController);
 
-routes.get('/car', carController.listAllCars.bind(carController));
-routes.get('/car/:_id', validateObjectId, carController.getCarById.bind(carController));
-routes.post('/car', validateCar, carController.createCar.bind(carController));
-routes.put('/car/:_id', validateObjectId, validateCar, carController.updateCar.bind(carController));
-routes.patch('/car/:_id/accessories/:accessoryId?', validateObjectId, carController.updateAccessory.bind(carController));
-routes.delete('/car/:_id', validateObjectId, carController.removeCar.bind(carController));
+routes.get('/car', checkToken, carController.listAllCars.bind(carController));
+routes.get('/car/:_id', checkToken, validateObjectId, carController.getCarById.bind(carController));
+routes.post('/car', checkToken, validateCar, carController.createCar.bind(carController));
+routes.put('/car/:_id', checkToken, validateObjectId, validateCar, carController.updateCar.bind(carController));
+routes.patch('/car/:_id/accessories/:accessoryId?',  checkToken, validateObjectId, carController.updateAccessory.bind(carController));
+routes.delete('/car/:_id', checkToken, validateObjectId, carController.removeCar.bind(carController));
 
 export default routes;
