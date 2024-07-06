2840836146818133c02ab3f76c48fe79d51290e1
TaliaMendes <taliafmendes09@gmail.com>
2024-07-05 21:42:33 -0300

    chore: Atualizar configuração de injeção de dependencia

--- src/config/dependency_injection.ts
+++ src/config/dependency_injection.ts
@@ -1,9 +1,22 @@
 import { container } from 'tsyringe';
 import { ICarRepository } from '../repositories.interface/Car.repositories.interface';
+import { IUserRepository } from '../repositories.interface/User.repositories.interface';
 import CarRepository from '../repositories/Car.repository';
-
+import UserRepository from '../repositories/User.repository';
+import { IReserveRepository } from '../repositories.interface/Reserve.repositories.interface';
+import ReserveRepository from '../repositories/Reserve.repository';
 
 container.registerSingleton<ICarRepository>(
   'CarRepository',
   CarRepository,
+);
+
+container.registerSingleton<IUserRepository>(
+  'UserRepository',
+  UserRepository,
+);
+
+container.registerSingleton<IReserveRepository>(
+  'ReserveRepository',
+  ReserveRepository,
 );
