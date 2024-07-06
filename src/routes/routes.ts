99901d5538a1c982a64f549380c8ba121edd66ed
TaliaMendes <taliafmendes09@gmail.com>
2024-07-05 21:23:52 -0300

    feat: Adicionar novas rotas e integrar com documentação do Swagger

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import carRouter from './Car.routes';
+import userRouter from './User.routes';
+import authRouter from './Authenticate.routes';
+import reserveRouter from './Reserve.routes';
+import swaggerDocument from '../Swagger/Swagger.json';
+import swaggerUi from 'swagger-ui-express';
 
 const routes = express.Router();
 
 routes.use('/api/v1/', carRouter);
-
+routes.use('/api/v1/', userRouter);
+routes.use('/api/v1/', authRouter);
+routes.use('/api/v1/', reserveRouter);
+routes.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 export default routes;
