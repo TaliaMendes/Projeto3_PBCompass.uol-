d965b9b87292234f7019ade615dc783cb00a9fdf
TaliaMendes <taliafmendes09@gmail.com>
2024-07-05 21:22:36 -0300

    refactor: Atualizar lógica de paginação e interface

--- src/interfaces/Pagination.interface.ts
+++ src/interfaces/Pagination.interface.ts
@@ -1,6 +1,7 @@
 export interface IPagination<type> {
-  data: type[];
-  total: number;
-  page: number;
-  pages: number;
+  data: type[] // Array de dados da página atual
+  total: number // Total de registros que correspondem aos filtros
+  limit: number // Limite de registros por página
+  offset: number // Total de páginas no total
+  offsets: number  // Número de páginas restantes após a página atual
 }
--- src/utils/Pagination.ts
+++ src/utils/Pagination.ts
@@ -1,20 +1,24 @@
 import { Model } from 'mongoose';
 import { IPagination } from '../interfaces/Pagination.interface';
 
-export async function paginateModel<type>( 
+export async function paginateModel<type>(
   model: Model<type>, 
   filters: object, 
-  page: number, 
-  limit: number
+  total: number,
+  limit: number,
+  page: number
 ): Promise<IPagination<type>> {
   try {
     const skip = (page - 1) * limit;
     const data = await model.find(filters).skip(skip).limit(limit);
-    const total = await model.countDocuments(filters);
-    const pages = Math.ceil(total / limit);
-    return { data, total, page, pages };
+    const totalCount = await model.countDocuments(filters);
+    const totalPages = Math.ceil(totalCount / limit);
+    const offsets = totalPages / page;
+
+    return { data, total: totalCount, limit, offset: totalPages, offsets };
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     throw new Error(`Error while paginating data: ${error.message}`);
   }
 }
+
